3001156c1e0103f292ed35d94f260c8e09b8de2c
Yorvin Luna Wisky <yorvin0506@gmail.com>
2020-05-13 20:24:04 -0400

    Principals components added, like bread, seeds, cheese, etc.

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import './App.css';
 
 class App extends Component {
@@ -8,7 +9,7 @@ class App extends Component {
     return (
       <div>
         <Layout>
-          <p>Test</p>
+          <BurgerBuilder />
         </Layout>
       </div >
     );
--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Aux from '../../hoc/Auxiliary';
+import classes from './Layout.module.css';
 const layout = (props) => (
   <Aux>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>
+    <main className={classes.Content}>
       {props.children}
     </main>
   </Aux>
