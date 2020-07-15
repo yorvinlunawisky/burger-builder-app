147b51a27537b860ff8cb7c4081bae4d78ab0bd5
yorvinlunawisky <yorvin0506@gmail.com>
2020-07-14 21:17:59 -0400

    Backdrop added

--- burger-builder/src/components/Logo/Logo.js
+++ burger-builder/src/components/Logo/Logo.js
@@ -3,7 +3,7 @@ import styles from './Logo.module.css';
 import burgerLogo from '../../assets/images/burger-logo.png';
 
 const logo = (props) => (
-  <div className={styles.Logo}>
+  <div className={styles.Logo} style={{ height: props.height }}>
     <img
       alt="burger logo"
       src={burgerLogo} />
--- burger-builder/src/components/Navigation/Toolbar/Toolbar.js
+++ burger-builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import styles from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = (props) => (
   <header className={styles.Toolbar}>
     <div>MENU</div>
-    <Logo />
-    <nav>
-      ...
+    <div className={styles.Logo}>
+      <Logo />
+    </div>
+    <nav className={styles.DesktopOnly}>
+      <NavigationItems />
     </nav>
   </header>
 );
